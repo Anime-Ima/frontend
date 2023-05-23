92ac0cf6a47045bf70c10f102cf9725be478cd45
david curtis <heythereimdavidcurtis@gmail.com>
2023-05-23 13:55:27 -0400

    set up Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import Table from './components/Table';
 import ExpenseList from './components/ExpenseList';
 import ExpenseFilter from './components/ExpenseFilter';
 import { useState } from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
 
 function App() {
-  return <div>TEST</div>;
+  return (
+    <div>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
 }
 
 export default App;
